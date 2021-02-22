e3be3d229f184d446eedfff3004346d1ba6a7bcd
Sasha <40319171+lonagi@users.noreply.github.com>
2021-02-22 18:37:43 +0200

    HomeBrew

--- install.zsh
+++ install.zsh
@@ -1,3 +1,9 @@
 #Sublime Text
 sudo mkdir -p /usr/local/bin
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl 
+
+#Install HomeBrew as NVG
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HE..)"
+export PATH=/opt/homebrew/bin:$PATH 
+echo '\nalias nvg="brew"' >> .zshrc
+nvg install coreutils
