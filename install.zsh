1fa134e9b06f876de0c8630d5586ee03fcffaf1c
Sasha <40319171+lonagi@users.noreply.github.com>
2021-02-23 03:09:54 +0200

    php

--- install.zsh
+++ install.zsh
@@ -7,6 +7,7 @@ zsh <(curl -Ls https://git.modder.pw/mcmodder/dotfiles-patches/raw/master/instal
 echo "\nexport PATH=/opt/homebrew/bin:$PATH" >> ~/.zsh/.zshrc.d/30-zsh.zsh
 echo '\nalias nvg="brew"' >> ~/.zsh/.zshrc.d/90-aliases.zsh
 nvg install coreutils
+sudo xcodebuild -license accept
 
 #Instal some soft
 nvg install curl htop neofetch lolcat cmake python3 ffmpeg unzip wget
@@ -15,9 +16,16 @@ pip3 install weboa
 #Beatf & cfgs
 sudo wget https://raw.githubusercontent.com/lonagi/my-linux/main/etc/motd -O /etc/motd.sh
 echo 'cat /etc/motd.sh | lolcat' >> ~/.zsh/.zshrc
-sudo wget "https://raw.githubusercontent.com/lonagi/fsociety/master/12.%20Configs/php.ini" -O /etc/php.ini
-nvg install pkg-config zlib
 
+#PHP
+nvg install php pkg-config zlib mysql
+sudo wget "https://raw.githubusercontent.com/lonagi/fsociety/master/12.%20Configs/php.ini" -O /etc/php.ini
 sudo mv composer.phar /usr/local/bin/composer
 curl -sS https://getcomposer.org/installer | php
-nvg install mysql
+wget https://files.phpmyadmin.net/phpMyAdmin/5.0.4/phpMyAdmin-5.0.4-all-languages.zip
+tar xvfz phpMyAdmin-5.0.4-all-languages.zip
+rm -rf phpMyAdmin-5.0.4-all-languages.zip
+sudo mv phpMyAdmin-5.0.4-all-languages /opt/phpmyadmin
+echo '\nalias phpmyadmin="/usr/bin/php -S localhost:8001 -t /opt/phpmyadmin"' >> ~/.zsh/.zshrc.d/90-aliases.zsh
+echo '\nalias nphpmyadmin="nohup /usr/bin/php -S localhost:8001 -t /opt/phpmyadmin &"' >> ~/.zsh/.zshrc.d/90-aliases.zsh
+cp /opt/phpmyadmin/config.sample.inc.php /opt/phpmyadmin/config.inc.php
