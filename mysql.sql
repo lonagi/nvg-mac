2a50e2f077fbac8edcf8d0d2fabd72d4bfd28236
Sasha <40319171+lonagi@users.noreply.github.com>
2021-02-23 03:09:43 +0200

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -1,3 +1,4 @@
 CREATE USER 'mroot'@'localhost' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON * . * TO 'mroot'@'localhost';
 FLUSH PRIVILEGES;
+ALTER USER 'mroot'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
