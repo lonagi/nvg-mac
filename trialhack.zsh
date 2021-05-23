043d3334e45c1da4b96023dea3f9f6cc44ac17a7
Sasha <40319171+lonagi@users.noreply.github.com>
2021-05-24 00:51:23 +0300

    Update trialhack.zsh

--- trialhack.zsh
+++ trialhack.zsh
@@ -1,7 +1,8 @@
 cd ~/Library/Preferences
 ls | grep 'jetbrains' | xargs rm
-rm ~/Library/Application Support/JetBrains/PhpStorm2020.3/eval/*.key
-rm ~/Library/Application Support/JetBrains/PhpStorm2020.3/options/other.xml
+rm ~/Library/Application\ Support/JetBrains/PhpStorm2020.3/eval/PhpStorm203.evaluation.key
+rm ~/Library/Application\ Support/JetBrains/PhpStorm2020.3/options/other.xml
+
 cd ~/Library/Preferences
 plutil -convert xml1 com.apple.java.util.prefs.plist
 sed -i'' -e '/evlsprt/d' com.apple.java.util.prefs.plist
